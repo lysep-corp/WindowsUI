607d970dae63cecda625febfbf5073b9edddaf37
Furkan Yaşar Çilkaya <53129220+Lufzys@users.noreply.github.com>
2021-03-21 23:02:11 +0300

    WinLabel added.

--- WindowsUI/Demo/MainForm.Designer.cs
+++ WindowsUI/Demo/MainForm.Designer.cs
@@ -37,6 +37,7 @@ namespace Demo
             this.winButton1 = new WindowsUI.WinButton();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.winLabel1 = new WindowsUI.Controls.WinLabel();
             this.pnlTop.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -131,7 +132,7 @@ namespace Demo
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(47, 58);
+            this.pictureBox1.Location = new System.Drawing.Point(52, 58);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(151, 163);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -185,11 +186,23 @@ namespace Demo
             this.pictureBox3.TabIndex = 7;
             this.pictureBox3.TabStop = false;
             // 
+            // winLabel1
+            // 
+            this.winLabel1.AutoSize = true;
+            this.winLabel1.Font = new System.Drawing.Font("Segoe UI Semibold", 9F);
+            this.winLabel1.ForeColor = System.Drawing.Color.White;
+            this.winLabel1.Location = new System.Drawing.Point(89, 40);
+            this.winLabel1.Name = "winLabel1";
+            this.winLabel1.Size = new System.Drawing.Size(75, 15);
+            this.winLabel1.TabIndex = 8;
+            this.winLabel1.Text = "ANTI - CORE";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(252, 370);
+            this.Controls.Add(this.winLabel1);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.winButton1);
@@ -213,11 +226,13 @@ namespace Demo
             this.Controls.SetChildIndex(this.winButton1, 0);
             this.Controls.SetChildIndex(this.pictureBox2, 0);
             this.Controls.SetChildIndex(this.pictureBox3, 0);
+            this.Controls.SetChildIndex(this.winLabel1, 0);
             this.pnlTop.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -229,5 +244,6 @@ namespace Demo
         private WindowsUI.WinButton winButton1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
+        private WindowsUI.Controls.WinLabel winLabel1;
     }
 }
---
+++ WindowsUI/WindowsUI/Controls/WinLabel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsUI.Controls
+{
+    public class WinLabel : Label
+    {
+        public WinLabel()
+        {
+            this.Font = new Font("Segoe UI Semibold", 9f);
+            this.ForeColor = Color.White;
+        }
+    }
+}
